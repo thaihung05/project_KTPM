f12fbdfb6c0d7e6d5119fdeeae5b70b9c685bdd1
VoHuynhThanhHuy <vohuynhthanhhuy91@gmail.com>
2026-03-24 20:58:47 +0700

    feat: implementation of book borrowing system with AJAX
    - Implement book borrowing logic with 5 business constraints (account status, 5-book limit, overdue check, stock availability, and user role)
    - Create borrow.js to handle borrowing requests
    - Integrate SweetAlert2 for notifications

--- library/eapp/__init__.py
+++ library/eapp/__init__.py
@@ -2,17 +2,12 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.secret_key = "passwordAbc123"
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:Abc123@localhost/librarydb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Abc123@localhost/librarydb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["due_time"] = 14
 app.config["PAGE_SIZE"] = 50
 
-
 db = SQLAlchemy(app)
 login = LoginManager(app=app)
-
-
-
